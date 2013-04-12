404ceadbeac8f0bfe8741cf2c314663256a0c17c
Mart van de Ven <m@type.hk>
2013-04-12 20:26:41 +0800

    * Initial Import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ODHK::Application.config.secret_token = 'f445070f656663524ef93595ed674653a411b4ed786e44db7d1a1a626db02d7dd6451661eb6866cf418d235b084897283e6b9a5589eddc5935320622d762e35e'
