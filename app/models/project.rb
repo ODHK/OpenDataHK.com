844ba2ab501471d3df24d5e3e7559fc768f3f650
Mart van de Ven <m@type.hk>
2013-05-17 00:17:56 +0800

    Added Github Fork & Watch buttons on Project Pages

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
-  attr_accessible :cover, :description, :title, :url, :stage, :users, :project_roles
+  attr_accessible :cover, :description, :title, :url, :stage, :users, :project_roles, 
+  				  :github_repo, :github_user
 
   has_many :users, :through => :project_roles
   has_many :project_roles, :foreign_key => :project_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130514010048) do
+ActiveRecord::Schema.define(:version => 20130516160829) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(:version => 20130514010048) do
     t.datetime "created_at",                          :null => false
     t.datetime "updated_at",                          :null => false
     t.string   "stage",       :default => "proposal"
+    t.string   "github_user", :default => "ODHK"
+    t.string   "github_repo"
   end
 
   create_table "resources", :force => true do |t|
