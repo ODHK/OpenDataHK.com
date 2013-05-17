4a81a90d084221673f5c57c8c33488433d0c14e4
Mart van de Ven <m@type.hk>
2013-05-17 11:06:40 +0800

    Midnight

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -31,7 +31,11 @@ def render_markdown(markup)
   def show
     @project = Project.find(params[:id])
     @project.description = render_markdown(@project.description)
-
+    @project["owner"] = @project.project_roles.detect{|p| p.role == "owner"}.user
+    @project["members"] = @project.project_roles.select{|p| p.role == "member"}
+    @users = User.all.map{|user| user.name}.to_json
+    ap @users
+    
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @project }
